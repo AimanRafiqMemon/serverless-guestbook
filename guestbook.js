6efc216970f74f67aa1d3eca47b6ead8d0329a96
AimanRafiqMemon <92150895+AimanRafiqMemon@users.noreply.github.com>
2022-02-10 09:05:41 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f18cd2e9.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
